b5487f30007987f4f5de232eba8e4b213067004c
Maxens Legoupil <maxens.legoupil13@gmail.com>
2024-09-24 17:00:30 +0200

    feat : timer bugé

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 var idInterval;
 var pause = false;
-var seconde = 59;
+var seconde = 0;
 var minute = 25;
+const timer = document.getElementById("timer");
+actualiseTimer();
 
-function passageTemps(){
+
+function passageTemps() {
     idInterval = setInterval(decrementeSeconde(), 1000);
+    console.log(idInterval);
 }
 
 function decrementeSeconde() {
+    console.log("test");
     if (seconde == 0) {
         seconde = 59;
         decrementeMinute();
     } else {
         seconde--;
     }
+    actualiseTimer()
 }
 
 function decrementeMinute() {
-    if(minute == 0){
+    console.log("test");
+    if (minute == 0) {
         passagePause();
     } else {
         minute--;
     }
+    actualiseTimer()
 }
 
-function passagePause(){
+function passagePause() {
     if (pause) {
         minute = 25;
     } else {
         minute = 5;
     }
+    actualiseTimer()
 }
 
-function stop(){
+function stop() {
     clearInterval(idInterval);
+    console.log("clear");
+}
+
+function actualiseTimer(){
+    console.log("test");
+    let partieMinute = "";
+    let partieSeconde = "";
+    if(minute < 10){
+        partieMinute = "0";
+    }
+    partieMinute += minute.toString();
+    if(seconde < 10){
+        partieSeconde = "0";
+    }
+    partieSeconde += seconde.toString();
+    timer.innerText = partieMinute + " : " + partieSeconde;
+    console.log("test");
 }
